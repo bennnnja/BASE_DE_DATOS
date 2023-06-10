26e3c181ac9fbd31db244dda7f028b50bc860c4d
bennnnja <115719198+bennnnja@users.noreply.github.com>
2023-06-10 19:10:36 -0400

    Add files via upload

---
+++ NUEVO PROYECTO BASE DE DATOS/conexion.php
@@ -0,0 +1,31 @@
+<?php
+$host = 'localhost'; // Normalmente es localhost
+$port = '5432'; // Por defecto es 5432
+$dbname = 'sistemalogin';
+$user = 'postgres';
+$password = 'password';
+//$nombre = $_POST['nombre'];
+//$edad = $_POST['edad'];
+//$correo = $_POST['correo'];
+// Establecer conexión
+$dsn =
+"pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$passw
+ord";
+try {
+$conexion = new PDO($dsn);
+// Consulta a una tabla específica
+//$sql = "INSERT INTO Persona () values() ";
+$sql = "SELECT * FROM usuario";
+$stmt = $conexion->query($sql);
+// Generar el contenido de la tabla HTML
+$tableContent = '';
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+$tableContent .= "<tr>";
+$tableContent .= "<td>" . $row['nombre_usuario'] . "</td>";
+$tableContent .= "<td>" . $row['email_usuario'] . "</td>";
+$tableContent .= "</tr>";
+}
+} catch (PDOException $e) {
+echo "Error al conectarse a la base de datos: " . $e-
+>getMessage();
+}
